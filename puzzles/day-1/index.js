ed8204de0e403ffb8d8cbcc6ed17f01728f8584d
Eduardo Paez Rubio <contact@devpaezrubio.com>
2020-12-01 19:15:11 +0200

    Add day 1 puzzle solution

---
+++ puzzles/day-1/index.js
@@ -0,0 +1,21 @@
+const InputReader = require('../../helpers/input-reader');
+
+function getAddingTupleMultiplication(target, input = []) {
+    const inverseAdditionMap = {}
+
+    for (let i = 0; i < input.length; i++) {
+        let n = input[i]
+
+        if (inverseAdditionMap[n]) {
+            return n * inverseAdditionMap[n]
+        }
+
+        // Add the pre-calculated matching number to the map
+        inverseAdditionMap[TARGET - n] = n
+    }
+}
+
+const TARGET = 2020;
+const input = InputReader.getDayReader('1').read(parseInt);
+
+console.log(getAddingTupleMultiplication(TARGET, input))
