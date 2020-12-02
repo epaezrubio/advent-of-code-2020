b071d8511a6d56d3ffce9343b2655390074968fa
Eduardo Paez Rubio <contact@devpaezrubio.com>
2020-12-02 23:11:01 +0200

    Add day 2 puzzle solution

---
+++ puzzles/day-2/index.js
@@ -0,0 +1,32 @@
+const InputReader = require('../../helpers/input-reader');
+
+function isValidPassword(requiredLetter, min, max, password) {
+    let letterCount = password.split('').reduce((acc, cur) => {
+        if (cur === requiredLetter) {
+            return acc + 1;
+        }
+
+        return acc;
+    }, 0);
+
+    return letterCount >= min && letterCount <= max;
+}
+
+function countValidPasswords(input) {
+    return input.reduce((acc, [letter, min, max, password]) => {
+        if (isValidPassword(letter, min, max, password)) {
+            return acc + 1;
+        }
+
+        return acc;
+    }, 0);
+}
+
+const input = InputReader.getDayReader('2').read((val) => {
+    const [range, letter, password] = val.split(' ');
+    const [min, max] = range.split('-')
+
+    return [letter[0], parseInt(min), parseInt(max), password]
+});
+
+console.log(countValidPasswords(input));
