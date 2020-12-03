1b46188ae0aace5cee26fb4247e3273ead42f382
Eduardo Paez Rubio <contact@devpaezrubio.com>
2020-12-03 22:44:51 +0200

    Add day 3 puzzle solution

---
+++ puzzles/day-3/index.js
@@ -0,0 +1,21 @@
+const InputReader = require('../../helpers/input-reader');
+
+function countTreeEncounters(right, bottom, treeGrid) {
+    let treeEncounters = 0;
+
+    for (let i = 0; i < treeGrid.length; i = i + bottom) {
+        let rightModulo = (right * i) % treeGrid[i].length
+
+        if (treeGrid[i][rightModulo] === '#') {
+            treeEncounters = treeEncounters + 1;
+        }
+    }
+
+    return treeEncounters;
+}
+
+const RIGHT = 3;
+const BOTTOM = 1;
+const input = InputReader.getDayReader('3').read();
+
+console.log(countTreeEncounters(RIGHT, BOTTOM, input));
