ea1d294f9ec166d49db67fcf33cf0b970cf842b5
Eduardo Paez Rubio <contact@devpaezrubio.com>
2020-12-11 19:10:56 +0200

    Add day 11 puzzle solution

---
+++ puzzles/day-11/part-1.js
@@ -0,0 +1,100 @@
+const InputReader = require('../../helpers/input-reader');
+
+function getCell (seats, x, y) {
+    // check if it's out of bounds
+    if (x < 0 || y < 0 || x >= seats[0].length || y >= seats.length) {
+        return null
+    }
+
+    return seats[y][x]
+}
+
+function countSurroundingCells (seats, x, y) {
+    let surroundingCounters = {
+        'L': 0,
+        '#': 0,
+        '.': 0
+    }
+
+    // loop the slice of the matrix of surrounding cells
+    for (let i = -1; i <= 1;  i++) {
+        for (let j = -1; j <= 1;  j++) {
+            // skip self
+            if (i === 0 && j === 0) {
+                continue
+            }
+
+            let neighbourCell = getCell(seats, x + j, y + i)
+
+            if (!neighbourCell) {
+                continue
+            }
+
+            surroundingCounters[neighbourCell] = surroundingCounters[neighbourCell] + 1
+        }
+    }
+
+    return surroundingCounters
+}
+
+function getNextSeatIteration (seats) {
+    let newSeats = []
+
+    seats.forEach((row, j) => {
+        let seatsList = row.split('')
+        let newSeatsRow = []
+
+        seatsList.forEach((seat, i) => {
+            let newState = seat;
+
+            if (seat === '.') {
+                newState = '.'
+            } else {
+                let surroundingSeatsCounts = countSurroundingCells(seats, i, j)
+
+                if (seat === 'L' && surroundingSeatsCounts['#'] === 0) {
+                    newState = '#'
+                } else if (seat === '#' && surroundingSeatsCounts['#'] >= 4) {
+                    newState = 'L'
+                }
+            }
+
+            newSeatsRow.push(newState)
+        })
+
+        newSeats.push(newSeatsRow.join(''))
+    })
+
+    return newSeats
+}
+
+function isUnchangedState(oldState, newState) {
+    return JSON.stringify(oldState) === JSON.stringify(newState)
+}
+
+function getOccupiedSeatsChangesIterations (seats) {
+    let lastState = null;
+    let newState = seats;
+    let iterations = 0
+
+    do {
+        lastState = newState
+        newState = getNextSeatIteration(lastState)
+        iterations = iterations + 1
+    } while (iterations < 200 && !isUnchangedState(lastState, newState));
+
+    // count occupied seats
+    return newState.reduce((acc, cur) => {
+        return acc + cur.split('').reduce((acc2, cur2) => {
+            if (cur2 === '#') {
+                return acc2 + 1
+            }
+
+            return acc2
+        }, 0)
+    }, 0)
+}
+
+const input = InputReader.getDayReader('11').read()
+
+console.log(getOccupiedSeatsChangesIterations(input))
