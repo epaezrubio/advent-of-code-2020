d7a1734a39d72d52b00f2dc5609d001b9c4a2ae2
Eduardo Paez Rubio <contact@devpaezrubio.com>
2020-12-17 01:10:02 +0200

    Add day 16 part 2 puzzle solution

--- puzzles/day-16/part-2.js
+++ puzzles/day-16/part-2.js
@@ -63,40 +63,122 @@ function parseNotes (notes) {
   return parsedNotes
 }
 
+function isValidRuleRange (range, field) {
+  return field >= range.min && field <= range.max
+}
+
+function isValidRule (rule, field) {
+  for (const range of rule) {
+    if (isValidRuleRange(range, field)) {
+      return true
+    }
+  }
+}
+
 function isValidField (rules, field) {
   for (const fieldRule in rules) {
-    for (const rule of rules[fieldRule]) {
-      if (field >= rule.min && field <= rule.max) {
-        return true
-      }
+    if (isValidRule(rules[fieldRule], field)) {
+      return true
     }
   }
 
   return false
 }
 
-function getInvalidFieldsRate (rules, ticket) {
-  const invalidFields = []
-
-  ticket.forEach((field) => {
-    if (!isValidField(rules, field)) {
-      invalidFields.push(field)
+function getValidTickets (rules, tickets) {
+  return tickets.filter((ticket) => {
+    for (let i = 0; i < ticket.length; i++) {
+      if (!isValidField(rules, ticket[i])) {
+        return false
+      }
     }
-  })
 
-  return invalidFields.reduce((acc, cur) => {
-    return acc + cur
-  }, 0)
+    return true
+  })
 }
 
-function getInvalidTicketsRate (notes) {
-  const { rules, nearbyTickets } = parseNotes(notes)
+function getRulesIndexes (rules, length) {
+  // get an array of booleans that represents in which indexes this rule is valid
+  const indexes = Array.from({ length }, (v, n) => true)
+  const rulesIndexes = {}
 
-  return nearbyTickets.reduce((acc, cur) => {
-    return acc + getInvalidFieldsRate(rules, cur)
-  }, 0)
+  for (const rule in rules) {
+    rulesIndexes[rule] = indexes.slice()
+  }
+
+  return rulesIndexes
+}
+
+function computeValidRulesIndexes (rules, rulesIndexes, tickets) {
+  // iterate over each ticket
+  tickets.forEach((ticket) => {
+    // iterate over each field
+    ticket.forEach((field, n) => {
+      // check if field is valid for each rule
+      for (const ruleKey in rules) {
+        // if it's invalid, mark it as invalid
+        if (!isValidRule(rules[ruleKey], field)) {
+          rulesIndexes[ruleKey][n] = false
+        }
+      }
+    })
+  })
+
+  return rulesIndexes
+}
+
+function recursiveDeduceRulesIndexes (rulesIndexes) {
+  let needsFurtherDeduction = false
+
+  for (const ruleKey in rulesIndexes) {
+    // find for how many fields this rule is valid
+    const validIndexes = rulesIndexes[ruleKey].reduce((acc, cur) => {
+      return acc + cur
+    }, 0)
+
+    // valid only for 1 rule, set the rest to false
+    if (validIndexes === 1) {
+      const validIndexPosition = rulesIndexes[ruleKey].indexOf(true)
+
+      for (const invalidRuleKey in rulesIndexes) {
+        // dont set itself to invalid
+        if (invalidRuleKey === ruleKey) {
+          continue
+        }
+
+        rulesIndexes[invalidRuleKey][validIndexPosition] = false
+      }
+    } else {
+      needsFurtherDeduction = true
+    }
+  }
+
+  if (needsFurtherDeduction) {
+    recursiveDeduceRulesIndexes(rulesIndexes)
+  }
+
+  return rulesIndexes
+}
+
+function getValidTicketsCalculation (notes) {
+  const { rules, myTicket, nearbyTickets } = parseNotes(notes)
+
+  const validTickets = getValidTickets(rules, nearbyTickets)
+  const rulesIndexes = getRulesIndexes(rules, validTickets[0].length)
+  const validRulesIndexes = computeValidRulesIndexes(rules, rulesIndexes, validTickets)
+  const deducedValidRules = recursiveDeduceRulesIndexes(validRulesIndexes)
+
+  return Object.entries(deducedValidRules).reduce((acc, [key, validRules]) => {
+    if (!key.startsWith('departure')) {
+      return acc
+    }
+
+    const validRuleIndex = validRules.indexOf(true)
+
+    return acc * myTicket[validRuleIndex]
+  }, 1)
 }
 
 const input = InputReader.getDayReader('16').read()
 
-console.log(getInvalidTicketsRate(input))
+console.log(getValidTicketsCalculation(input))
